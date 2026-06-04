05a49ef6b01a24aafa8aaa55e829e3bf4e2dd884
yuki-devlab <yuki61051108@gmail.com>
2026-06-04 23:01:14 +0900

    refactor: add suspense

--- src/app/generate/result/page.tsx
+++ src/app/generate/result/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import type { Metadata } from "next";
 import ResultClient from "@/app/generate/result/ResultClient";
 
@@ -7,6 +8,8 @@ export const metadata: Metadata = {
 
 export default function ResultPage() {
     return (
-        <ResultClient />
+        <Suspense fallback={<div>Loading...</div>}>
+            <ResultClient />
+        </Suspense>
     )
 }
