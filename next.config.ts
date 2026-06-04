53cdf3e322db31acf75e841248ebcfb5c4b08ce3
yuki-devlab <yuki61051108@gmail.com>
2026-06-05 01:44:55 +0900

    refactor: adjust next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     devIndicators: false,
     reactCompiler: true,
+    serverExternalPackages: ["playwright-core", "playwright"],
     images: {
         remotePatterns: [
             {
