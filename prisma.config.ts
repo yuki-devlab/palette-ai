56e3459844633903e674a1214893978c8285731c
yuki-devlab <yuki61051108@gmail.com>
2026-06-12 22:00:52 +0900

    fix: use DIRECT_URL

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
     datasource: {
-        url: env("DATABASE_URL"),
+        url: env("DIRECT_URL"),
     },
     schema: "prisma/schema.prisma",
     migrations: {
