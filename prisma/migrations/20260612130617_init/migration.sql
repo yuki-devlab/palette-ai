40ae37c06d32ae311dcd5a970e56b5f1181d0eda
yuki-devlab <yuki61051108@gmail.com>
2026-06-13 01:18:29 +0900

    refactor: recreate prisma migrations

--- prisma/migrations/20260612130617_init/migration.sql
+++ prisma/migrations/20260612130617_init/migration.sql
@@ -1,17 +1,17 @@
 -- CreateTable
 CREATE TABLE "accounts" (
     "id" TEXT NOT NULL,
-    "user_id" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "id_token" TEXT,
     "provider" TEXT NOT NULL,
     "provider_account_id" TEXT NOT NULL,
     "refresh_token" TEXT,
-    "access_token" TEXT,
-    "expires_at" INTEGER,
-    "token_type" TEXT,
     "scope" TEXT,
-    "id_token" TEXT,
     "session_state" TEXT,
+    "token_type" TEXT,
+    "type" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
 
     CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
 );
@@ -19,9 +19,9 @@ CREATE TABLE "accounts" (
 -- CreateTable
 CREATE TABLE "sessions" (
     "id" TEXT NOT NULL,
-    "session_token" TEXT NOT NULL,
-    "user_id" TEXT NOT NULL,
     "expires" TIMESTAMP(3) NOT NULL,
+    "session_state" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
 
     CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
 );
@@ -29,26 +29,39 @@ CREATE TABLE "sessions" (
 -- CreateTable
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
-    "name" TEXT,
     "email" TEXT,
     "email_verified" TIMESTAMP(3),
     "image" TEXT,
+    "name" TEXT,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "verification_tokens" (
+    "expires" TIMESTAMP(3) NOT NULL,
     "identifier" TEXT NOT NULL,
-    "token" TEXT NOT NULL,
-    "expires" TIMESTAMP(3) NOT NULL
+    "token" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "histories" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "mode" TEXT NOT NULL,
+    "base_color" TEXT NOT NULL,
+    "main_color" TEXT NOT NULL,
+    "accent_color" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "histories_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "accounts_provider_provider_account_id_key" ON "accounts"("provider", "provider_account_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "sessions_session_token_key" ON "sessions"("session_token");
+CREATE UNIQUE INDEX "sessions_session_state_key" ON "sessions"("session_state");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
@@ -61,3 +74,6 @@ ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user
 
 -- AddForeignKey
 ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "histories" ADD CONSTRAINT "histories_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
