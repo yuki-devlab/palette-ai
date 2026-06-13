11860934e5894b103ecde7713c7b5f9a0cb2bccd
yuki-devlab <yuki61051108@gmail.com>
2026-06-13 18:23:46 +0900

    refactor: clean up prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,18 @@
-import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL!,
-});
+import { PrismaClient } from "@/app/generated/prisma/client";
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient;
-};
+}
 
-const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        adapter,
-    });
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+})
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
