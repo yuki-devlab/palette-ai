63a9e5b65c60e9e2fc84620246cde4937b2141c7
yuki-devlab <yuki61051108@gmail.com>
2026-06-20 18:14:29 +0900

    feat: add proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,43 +1,8 @@
-import { NextRequest, NextResponse } from "next/server";
+import createMiddleware from "next-intl/middleware";
+import { routing } from "@/i18n/routing";
 
 export const config = {
-    matcher: ["/:path*"],
-};
-
-export function proxy(req: NextRequest) {
-    if (process.env.ENABLE_BASIC_AUTH !== "true") {
-        return NextResponse.next();
-    }
-
-    if (
-        process.env.BASIC_AUTH_USERNAME === undefined ||
-        process.env.BASIC_AUTH_PASSWORD === undefined
-    ) {
-        return NextResponse.next();
-    }
-
-    const basicAuth = req.headers.get("authorization");
-
-    if (basicAuth) {
-        const authValue = basicAuth.split(" ")[1];
-        const [username, password] = Buffer.from(authValue, "base64")
-            .toString()
-            .split(":");
-
-        if (
-            username === process.env.BASIC_AUTH_USERNAME &&
-            password === process.env.BASIC_AUTH_PASSWORD
-        ) {
-            return NextResponse.next();
-        }
-    }
-
-    return NextResponse.json(
-        { error: "Basic Auth Required" },
-        {
-            // eslint-disable-next-line quotes
-            headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
-            status: 401,
-        }
-    );
+    matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
 }
+
+export default createMiddleware(routing);
