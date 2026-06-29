233440d2ad206359153db9563052d1b0821277b5
yuki-devlab <yuki61051108@gmail.com>
2026-06-30 01:37:21 +0900

    refactor: clean up LoginButton.tsx

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -8,18 +8,27 @@ export default function LoginButton() {
     const t = useTranslations("sidebar");
     const [modalIsOpen, setModalIsOpen] = useState(false);
 
+    const openModal = () => {
+        setModalIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    };
+
     return (
         <>
             <button
+                type="button"
+                onClick={openModal}
                 className="bg-sky-500 font-bold py-6 rounded-full [text-box:trim-both_cap_alphabetic] text-sm text-white hover:bg-blue-500"
-                onClick={() => setModalIsOpen(true)}
             >
                 {t("login")}
             </button>
             <LoginModal
                 isOpen={modalIsOpen}
-                onClose={() => setModalIsOpen(false)}
+                onClose={closeModal}
             />
         </>
-    )
+    );
 }
