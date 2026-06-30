4f13b453768be70b23a7b84c996c8a58774cf4ad
yuki-devlab <yuki61051108@gmail.com>
2026-06-30 19:32:38 +0900

    feat: setup next-intl

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,22 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
     devIndicators: false,
-    reactCompiler: true,
-    serverExternalPackages: ["playwright-core", "playwright"],
     images: {
         remotePatterns: [
             {
                 protocol: "https",
                 hostname: "lh3.googleusercontent.com",
                 port: "",
-                pathname: "/**",
-            }
-        ]
-    }
-}
+                pathname: "/a/**",
+                search: "",
+            },
+        ],
+    },
+    reactCompiler: true,
+};
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+
+export default withNextIntl(nextConfig);
