a883bd92da343b5b6a6d3faa643bbd61c328498e
yuki-devlab <yuki61051108@gmail.com>
2026-06-30 10:52:47 +0900

    feat: add ModalLoginButton.tsx

---
+++ src/components/login-button/ModalLoginButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+import Image from "next/image";
+import { useTranslations } from "next-intl";
+
+export default function ModalLoginButton() {
+    const t = useTranslations("loginModal");
+    const { pending } = useFormStatus();
+
+    return (
+        <button
+            type="submit"
+            disabled={pending}
+            className="bg-google-login flex gap-3 items-center justify-center py-5 rounded-full w-full hover:bg-slate-800"
+        >
+            {pending ? (
+                <div className="animate-spin border-[3px] border-t-white border-white/25 h-5 rounded-full w-5" />
+            ) : (
+                <Image
+                    src="/google-logo.svg"
+                    width={20}
+                    height={20}
+                    alt=""
+                />
+            )}
+            <span className="font-bold [text-box:trim-both_cap_alphabetic] text-white">
+                {t("login")}
+            </span>
+        </button>
+    );
+}
