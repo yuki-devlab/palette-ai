8ce641dc6559575f88a9901217aac82052a01bd6
yuki-devlab <yuki61051108@gmail.com>
2026-06-30 11:02:23 +0900

    feat: add SidebarLoginButton.tsx

---
+++ src/components/login-button/SidebarLoginButton.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+import { useTranslations } from "next-intl";
+import LoginModal from "@/components/LoginModal";
+
+export default function SidebarLoginButton() {
+    const t = useTranslations("sidebar");
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const openModal = () => {
+        setModalIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    };
+
+    return (
+        <>
+            <button
+                type="button"
+                onClick={openModal}
+                className="bg-sky-500 font-bold py-6 rounded-full [text-box:trim-both_cap_alphabetic] text-sm text-white hover:bg-blue-500"
+            >
+                {t("login")}
+            </button>
+            <LoginModal
+                isOpen={modalIsOpen}
+                onClose={closeModal}
+            />
+        </>
+    );
+}
