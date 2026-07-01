f63e91b4a75b54628e89e4d2a4f896677914b87a
yuki-devlab <yuki61051108@gmail.com>
2026-07-01 13:26:47 +0900

    feat: add navigation.ts

---
+++ src/i18n/navigation.ts
@@ -0,0 +1,10 @@
+import { createNavigation } from "next-intl/navigation";
+import { routing } from "@/i18n/routing";
+
+export const {
+    getPathname,
+    Link,
+    redirect,
+    usePathname,
+    useRouter,
+} = createNavigation(routing);
