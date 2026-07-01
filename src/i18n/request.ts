7cd076ff0da7115ba2b0ce827544d6d2e6b35a27
yuki-devlab <yuki61051108@gmail.com>
2026-07-01 15:48:00 +0900

    chore: format request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,5 +1,5 @@
-import { getRequestConfig } from "next-intl/server";
 import { hasLocale } from "next-intl";
+import { getRequestConfig } from "next-intl/server";
 import { routing } from "@/i18n/routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
@@ -11,5 +11,5 @@ export default getRequestConfig(async ({ requestLocale }) => {
     return {
         locale,
         messages: (await import(`~/messages/${locale}.json`)).default,
-    }
-})
+    };
+});
