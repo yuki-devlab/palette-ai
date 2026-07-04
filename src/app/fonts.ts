f3ca6578e4d0e53473ee8d2a141759f24998b103
yuki-devlab <yuki61051108@gmail.com>
2026-07-05 03:09:14 +0900

    feat: add fonts.ts

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Noto_Sans_JP } from "next/font/google";
+
+export const inter = Inter({
+    subsets: ["latin"],
+    variable: "--font-inter",
+});
+
+export const notoSansJP = Noto_Sans_JP({
+    subsets: ["latin"],
+    variable: "--font-noto-sans-jp",
+});
