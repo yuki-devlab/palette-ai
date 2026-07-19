f8e652a0593aab93d2fc655c028f97b3a5089969
yuki-devlab <yuki61051108@gmail.com>
2026-07-19 18:18:27 +0900

    style: reorder imports

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
-import Google from "next-auth/providers/google";
-import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
 import prisma from "@/lib/prisma";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
