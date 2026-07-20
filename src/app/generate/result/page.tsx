f8f20c8676a64b2a280fe4c720d1487673a59f33
yuki-devlab <yuki61051108@gmail.com>
2026-07-21 04:20:35 +0900

    refactor: delete metadata settings

--- src/app/generate/result/page.tsx
+++ src/app/generate/result/page.tsx
@@ -1,27 +1,4 @@
-import type { Metadata } from "next";
-import { createMetadata } from "@/lib/metadata";
 import ResultClient from "@/components/result/ResultClient";
-
-type GenerateMetadataProps = {
-    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-};
-
-export async function generateMetadata({ searchParams }: GenerateMetadataProps): Promise<Metadata> {
-    const { id } = await searchParams;
-
-    const idString = typeof id === "string" ? id: "";
-
-    const dynamicMetadata = createMetadata({
-        pathname: `/generate/result${idString ? `?id=${idString}` : ""}`,
-    });
-
-    return {
-        ...dynamicMetadata,
-        description: "Palette AIの生成結果ページです。生成された配色はこちらになります。",
-        robots: { index: false },
-        title: "生成結果",
-    }
-}
 
 export default function ResultPage() {
     return (
