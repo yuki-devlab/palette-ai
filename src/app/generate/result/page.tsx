61f4fa1dc3c0c7887140e14ab0f889d6e1bc7b9e
yuki-devlab <yuki61051108@gmail.com>
2026-07-21 04:52:20 +0900

    refactor: add Suspense

--- src/app/generate/result/page.tsx
+++ src/app/generate/result/page.tsx
@@ -1,7 +1,10 @@
+import { Suspense } from "react";
 import ResultClient from "@/components/result/ResultClient";
 
 export default function ResultPage() {
     return (
-        <ResultClient />
+        <Suspense>
+            <ResultClient />
+        </Suspense>
     );
 }
