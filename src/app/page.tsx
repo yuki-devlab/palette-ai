b1b3624ecfc652bc9d48ff5adc6963dad0ce5af4
yuki-devlab <yuki61051108@gmail.com>
2026-07-26 07:47:35 +0900

    style: update home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,45 @@
-import { KeyboardDoubleArrowDown as KeyboardDoubleArrowDownW700 } from "@material-symbols-svg/react/w700";
-import ModeCarousel from "@/components/ModeCarousel";
+import { cn } from "@/lib/utils";
+import ArrowIcon from "@/components/icons/ArrowIcon";
+import ModeCarousel from "@/components/ui/ModeCarousel";
 
 export default function HomePage() {
     return (
-        <div className={`
-            flex flex-col gap-7 h-full items-center justify-center
-            md:gap-8
-            lg:gap-10
-        `}>
-            <div className={`
-                flex flex-col gap-7 items-center px-4
-                md:gap-8 md:p-0
-            `}>
-                <div className="w-50 h-13 bg-slate-200" />
-                <p className={`
-                    leading-loose [text-box:trim-both_cap_alphabetic] text-center text-slate-500 text-sm
-                    lg:text-base
-                `}>
+        <div className={cn(
+            "flex flex-col gap-8 items-center",
+            "md:gap-11",
+            "lg:gap-9",
+            "xl:gap-11",
+        )}>
+            <div className={cn(
+                "flex flex-col gap-7 items-center px-5",
+                "md:gap-8 md:px-0",
+                "lg:gap-6",
+                "xl:gap-8",
+            )}>
+                <div className={cn(
+                    "w-33.5 h-8 bg-[#D9D9D9]",
+                    "md:w-37.5 md:h-9",
+                    "lg:w-29.25 lg:h-7",
+                    "xl:w-37.5 xl:h-9",
+                )} />
+                <p className={cn(
+                    "[text-box:trim-both_cap_alphabetic] text-center text-slate-500 text-xs leading-loose",
+                    "md:text-sm",
+                    "lg:text-xs",
+                    "xl:text-base",
+                )}>
                     Palette AIは、ベースカラー・メインカラー・アクセントカラーを自動生成するAIツールです。
                     <br />
                     配色の知識がない方でも、簡単に生成することができます。
                 </p>
             </div>
-            <KeyboardDoubleArrowDownW700
-                size={40}
-                color="var(--color-sky-500)"
-                className="animate-bounce"
+            <ArrowIcon
+                className={cn(
+                    "h-13",
+                    "md:h-16",
+                    "lg:h-12",
+                    "xl:h-16",
+                )}
             />
             <ModeCarousel />
         </div>
