0f5f139868b0400457ee6238b46e935477647339
yuki-devlab <yuki61051108@gmail.com>
2026-07-29 01:57:06 +0900

    feat: add customer-portal.ts

---
+++ src/actions/customer-portal.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { stripe } from "@/lib/stripe";
+
+export async function createCustomerPortalSession() {
+    const session = await auth();
+
+    if (!session?.user.id) {
+        throw new Error("ログインしてください");
+    }
+
+    const subscription = await prisma.subscription.findUnique({
+        where: {
+            userId: session.user.id,
+        },
+        select: {
+            customerId: true,
+        },
+    });
+
+    if (!subscription?.customerId) {
+        throw new Error("ご契約中のプランが見つかりません");
+    }
+
+    const portalSession = await stripe.billingPortal.sessions.create({
+        customer: subscription.customerId,
+        return_url: `${process.env.NEXT_PUBLIC_APP_URL}`,
+    });
+
+    return { url: portalSession.url };
+}
