d0b412282406fb06dfa4b2db1934f217863acc21
yuki-devlab <yuki61051108@gmail.com>
2026-07-29 05:13:32 +0900

    feat: add delete-account.ts

---
+++ src/actions/delete-account.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { auth, signOut } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { stripe } from "@/lib/stripe";
+
+export async function deleteAccount() {
+    const session = await auth();
+
+    if (!session?.user.id) {
+        throw new Error("ログインしてください");
+    }
+
+    try {
+        const subscription = await prisma.subscription.findUnique({
+            where: {
+                userId: session.user.id,
+            },
+            select: {
+                subscriptionId: true,
+            },
+        });
+
+        if (subscription?.subscriptionId) {
+            await stripe.subscriptions.cancel(subscription.subscriptionId);
+        }
+
+        await prisma.user.delete({
+            where: {
+                id: session.user.id,
+            },
+        });
+    } catch (error) {
+        throw new Error("アカウントの削除に失敗しました");
+    }
+
+    await signOut({ redirectTo: "/" });
+}
