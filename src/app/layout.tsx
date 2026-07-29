1e93beea6b5d54bcc9765e65169245d546bb8f7c
yuki-devlab <yuki61051108@gmail.com>
2026-07-30 00:07:54 +0900

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,36 @@
+import type { Metadata, Viewport } from "next";
 import "@/app/globals.css";
 import { inter, notoSansJP } from "@/app/fonts";
 import { cn } from "@/lib/utils";
 import Footer from "@/components/layout/Footer";
-import Header from "@/components/Header";
+import Header from "@/components/layout/header/Header";
 import Sidebar from "@/components/layout/sidebar/Sidebar";
 
 type RootLayoutProps = {
     children: React.ReactNode;
+};
+
+export const viewport: Viewport = {
+    themeColor: "#DFF2FE",
+};
+
+export const metadata: Metadata = {
+    appleWebApp: {
+        title: "Palette AI",
+    },
+    description: "Palette AIは、ベースカラー・メインカラー・アクセントカラーを自動生成するAIツールです。配色の知識がない方でも、簡単に生成することができます。",
+    metadataBase: new URL("https://palette-ai.jp"),
+    openGraph: {
+        siteName: "Palette AI",
+        type: "website",
+    },
+    title: {
+        default: "Palette AI | クリエイターのための配色支援ツール",
+        template: "%s | Palette AI",
+    },
+    verification: {
+        google: "pcO7KAV3YtM4v2Nr0fXYE6Zx9RIGMtkQAR27MVFEH0w",
+    },
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
