13892dedb868cbe77cb5afd5d06a67433a2a6e40
yuki-devlab <yuki61051108@gmail.com>
2026-07-29 15:28:22 +0900

    refactor: update Tooltip.tsx

---
+++ src/components/layout/sidebar/_components/Tooltip.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { HelpFill } from "@material-symbols-svg/react";
+import { cn } from "@/lib/utils";
+
+export default function Tooltip() {
+    const [isOpen, setIsOpen] = useState(false);
+    const tooltipRef = useRef<HTMLDivElement>(null);
+
+    const toggleTooltip = () => {
+        if (
+            typeof window !== "undefined" &&
+            window.innerWidth >= 1280
+        ) {
+            return;
+        }
+
+        setIsOpen((prev) => !prev);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event: PointerEvent) => {
+            if (
+                tooltipRef.current &&
+                !tooltipRef.current.contains(event.target as Node)
+            ) {
+                setIsOpen(false);
+            }
+        };
+
+        document.addEventListener("pointerdown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("pointerdown", handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <div
+            ref={tooltipRef}
+            className="group relative"
+        >
+            <HelpFill
+                className={cn(
+                    "h-3.75 text-slate-400 w-3.75",
+                    "hover:text-slate-500",
+                    "xl:h-4.25 xl:w-4.25"
+                )}
+                onClick={toggleTooltip}
+            />
+            <div className={cn(
+                "absolute bg-slate-800 hidden left-1/2 px-2 py-3 rounded-xs text-[8px] [text-box:trim-both_cap_alphabetic] text-white top-[calc(100%+16px)] -translate-x-1/2 w-30",
+                "before:absolute before:border-b-8 before:border-b-slate-800 before:border-x-8 before:border-x-transparent before:bottom-full before:content-[''] before:left-1/2 before:-translate-x-1/2",
+                isOpen && "block",
+                "xl:p-3 xl:text-[10px] xl:leading-relaxed xl:w-35 xl:group-hover:block",
+            )}>
+                ログインすると生成履歴を保存できます
+            </div>
+        </div>
+    );
+}
