012bcabdcb2cca304e2ba459ca9f899dfaaacb83
yuki-devlab <yuki61051108@gmail.com>
2026-08-05 14:51:41 +0900

    refactor: simplify Header.tsx

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,34 +1,16 @@
 import Link from "next/link";
 import { auth } from "@/lib/auth";
-import prisma from "@/lib/prisma";
 import { cn } from "@/lib/utils";
+import AuthButton from "@/components/layout/header/_components/AuthButton";
 import HamburgerMenu from "@/components/layout/header/_components/HamburgerMenu";
-import LoginButton from "@/components/layout/header/_components/LoginButton";
-import ProfileButton from "@/components/layout/header/_components/ProfileButton";
 
 export default async function Header() {
     const session = await auth();
-    let isPro = false;
-
-    if (session?.user.id) {
-        const subscription = await prisma.subscription.findUnique({
-            where: {
-                userId: session?.user.id,
-            },
-            select: {
-                currentPeriodEnd: true,
-            },
-        });
-
-        isPro =
-            !!subscription?.currentPeriodEnd &&
-            subscription.currentPeriodEnd > new Date();
-    }
 
     return (
         <header className={cn(
-            "bg-white flex h-14 items-center justify-between px-4 relative rounded-2xl shadow",
-            "md:mx-auto md:w-lg",
+            "bg-white flex items-center justify-between px-4 py-2.5 relative rounded-[20px] shadow-sm w-full",
+            "md:w-lg",
             "lg:hidden",
         )}>
             <HamburgerMenu />
@@ -36,16 +18,9 @@ export default async function Header() {
                 href="/"
                 className="absolute left-1/2 -translate-x-1/2"
             >
-                <div className="w-9 h-9 rounded bg-[#D9D9D9]" />
+                <div className="w-10 h-10 rounded-lg bg-slate-300" />
             </Link>
-            {session?.user ? (
-                <ProfileButton
-                    user={session.user}
-                    isPro={isPro}
-                />
-            ) : (
-                <LoginButton />
-            )}
+            <AuthButton user={session?.user} />
         </header>
     );
 }
