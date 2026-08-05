c43459cefa8020d784a27db9f081baaa587281d0
yuki-devlab <yuki61051108@gmail.com>
2026-08-06 01:16:14 +0900

    feat: add login modal

--- src/components/layout/header/_components/LoginButton.tsx
+++ src/components/layout/header/_components/LoginButton.tsx
@@ -1,15 +1,36 @@
+"use client";
+
+import { useState } from "react";
 import { cn } from "@/lib/utils";
+import LoginModal from "@/components/ui/modal/login/LoginModal";
 
 export default function LoginButton() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openModal = () => {
+        setIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsOpen(false);
+    };
+
     return (
-        <button
-            type="button"
-            className={cn(
-                "bg-slate-100 font-bold h-10 px-4 rounded-xl text-sm",
-                "hover:bg-slate-200",
-            )}
-        >
-            ログイン
-        </button>
+        <>
+            <button
+                type="button"
+                className={cn(
+                    "bg-slate-100 font-bold h-10 px-4 rounded-xl text-sm",
+                    "hover:bg-slate-200",
+                )}
+                onClick={openModal}
+            >
+                ログイン
+            </button>
+            <LoginModal
+                isOpen={isOpen}
+                onClose={closeModal}
+            />
+        </>
     );
 }
