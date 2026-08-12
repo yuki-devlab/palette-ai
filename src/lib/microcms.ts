9e48e37948bc0f847001808d82be81ae0e44aa50
yuki-devlab <yuki61051108@gmail.com>
2026-08-12 19:09:30 +0900

    feat: add microcms fetch

--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -1,6 +1,26 @@
 import { createClient } from "microcms-js-sdk";
+import { Category, News } from "@/types/news";
 
 export const client = createClient({
     serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN || "",
     apiKey: process.env.MICROCMS_API_KEY || "",
 });
+
+export async function getCategories() {
+    const data = await client.getList<Category>({
+        endpoint: "categories",
+    });
+
+    return data.contents;
+}
+
+export async function getNews() {
+    const data = await client.getList<News>({
+        endpoint: "news",
+        queries: {
+            orders: "-publishedAt",
+        },
+    });
+
+    return data.contents;
+}
