2d0a24bab5d602a2c7a0bfdda6bf10dc5935cd38
yuki-devlab <yuki61051108@gmail.com>
2026-08-15 02:15:18 +0900

    refactor: update footer component

---
+++ src/components/layout/footer/Footer.tsx
@@ -0,0 +1,51 @@
+import { navigation } from "@/components/layout/footer/_data/navigation";
+import { cn } from "@/lib/utils";
+import FooterNav from "@/components/layout/footer/_components/FooterNav";
+
+export default function Footer() {
+    return (
+        <footer className={cn(
+            "flex justify-center pt-16 px-5",
+            "xl:px-0",
+        )}>
+            <div className={cn(
+                "flex flex-col gap-12 w-full",
+                "xl:w-4xl",
+            )}>
+                <div className={cn(
+                    "flex flex-col gap-12",
+                    "xl:flex-row xl:justify-between",
+                )}>
+                    <div className="flex flex-col gap-4">
+                        <div className="w-30 h-7 bg-[#D9D9D9]" />
+                        <span className="leading-normal [text-box:trim-both_cap_alphabetic] text-slate-500 text-sm">
+                            クリエイターのための
+                            <br className={cn(
+                                "hidden",
+                                "xl:block",
+                            )} />
+                            配色支援ツール
+                        </span>
+                    </div>
+                    <div className={cn(
+                        "gap-10 grid grid-cols-2",
+                        "md:gap-16 md:grid-cols-3",
+                    )}>
+                        {navigation.map((item) => (
+                            <FooterNav
+                                key={item.id}
+                                category={item.category}
+                                links={item.links}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <div className="border-slate-300 border-t flex justify-center py-6">
+                    <small className="[text-box:trim-both_cap_alphabetic] text-slate-500 text-sm">
+                        &copy; 2026 Palette AI
+                    </small>
+                </div>
+            </div>
+        </footer>
+    );
+}
---
+++ src/components/layout/footer/_data/navigation.ts
@@ -0,0 +1,52 @@
+export const navigation = [
+    {
+        id: "about",
+        category: "About",
+        links: [
+            {
+                title: "Palette AIについて",
+                url: "/about",
+            },
+            {
+                title: "お知らせ・更新情報",
+                url: "/news",
+            },
+            {
+                title: "料金プラン",
+                url: "/pricing",
+            },
+        ],
+    },
+    {
+        id: "legal",
+        category: "Legal",
+        links: [
+            {
+                title: "利用規約",
+                url: "/terms",
+            },
+            {
+                title: "プライバシーポリシー",
+                url: "/privacy",
+            },
+            {
+                title: "特商法表記",
+                url: "/commercial-transactions",
+            },
+        ],
+    },
+    {
+        id: "support",
+        category: "Support",
+        links: [
+            {
+                title: "よくある質問",
+                url: "/faq",
+            },
+            {
+                title: "お問い合わせ",
+                url: "/contact",
+            },
+        ],
+    },
+] as const;
