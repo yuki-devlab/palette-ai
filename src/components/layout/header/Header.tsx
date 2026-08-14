13145256ee319cb96f98c951c4b16c6fa923c31a
yuki-devlab <yuki61051108@gmail.com>
2026-08-14 16:06:59 +0900

    style: update header layout

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -9,16 +9,15 @@ export default async function Header() {
 
     return (
         <header className={cn(
-            "bg-white flex items-center justify-between px-4 py-2 relative rounded-[20px] shadow-sm w-full",
+            "bg-white flex items-center justify-between px-4 py-3 rounded-3xl shadow-sm w-full",
             "md:w-lg",
-            "lg:hidden",
         )}>
             <HamburgerMenu />
             <Link
                 href="/"
                 className="absolute left-1/2 -translate-x-1/2"
             >
-                <div className="w-10 h-10 rounded-lg bg-slate-300" />
+                <div className="w-10 h-10 rounded-lg bg-[#D9D9D9]" />
             </Link>
             <AuthButton user={session?.user} />
         </header>
