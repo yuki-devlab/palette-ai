919f0f1aa8bcb5c4032c4dd56e629f86a5bd9b9d
yuki-devlab <yuki61051108@gmail.com>
2026-08-16 01:29:35 +0900

    feat: add UpgradeModal.tsx

--- src/components/ui/modal/upgrade/UpgradeModal.tsx
+++ src/components/ui/modal/upgrade/UpgradeModal.tsx
@@ -1,19 +1,23 @@
+"use client";
+
 import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import Link from "next/link";
-import { Close } from "@material-symbols-svg/react/w700";
 import { cn } from "@/lib/utils";
-import SubscribeButton from "@/components/ui/modal/upgrade/_components/SubscribeButton";
+import PricingCard from "@/components/ui/modal/upgrade/_components/PricingCard";
 import ToggleButton from "@/components/ui/modal/upgrade/_components/ToggleButton";
 
 type UpgradeModalProps = {
     isOpen: boolean;
     onClose: () => void;
+    isPro: boolean;
 };
 
-export default function UpgradeModal({ isOpen, onClose }: UpgradeModalProps) {
+export default function UpgradeModal({ isOpen, onClose, isPro }: UpgradeModalProps) {
     const [isYearly, setIsYearly] = useState(true);
     const [mounted, setMounted] = useState(false);
+    const [isRendered, setIsRendered] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
 
     useEffect(() => {
         setMounted(true);
@@ -21,17 +25,27 @@ export default function UpgradeModal({ isOpen, onClose }: UpgradeModalProps) {
 
     useEffect(() => {
         if (isOpen) {
+            setIsRendered(true);
             document.body.style.overflow = "hidden";
-        } else {
-            document.body.style.overflow = "";
-        }
 
-        return () => {
+            const timer = setTimeout(() => {
+                setIsVisible(true);
+            }, 10);
+
+            return () => clearTimeout(timer);
+        } else {
+            setIsVisible(false);
             document.body.style.overflow = "";
-        };
+
+            const timer = setTimeout(() => {
+                setIsRendered(false);
+            }, 150);
+
+            return () => clearTimeout(timer);
+        }
     }, [isOpen]);
 
-    if (!mounted) {
+    if (!mounted || !isRendered) {
         return null;
     }
 
@@ -44,120 +58,35 @@ export default function UpgradeModal({ isOpen, onClose }: UpgradeModalProps) {
     return createPortal(
         <div className={cn(
             "fixed flex inset-0 items-center justify-center px-5",
-            isOpen ? "pointer-events-auto" : "pointer-events-none",
+            isVisible ? "pointer-events-auto" : "pointer-events-none",
             "md:p-0",
         )}>
             <div
-                className={cn(
-                    "backdrop-blur-md bg-slate-500/25 fixed inset-0",
-                    isOpen ? "opacity-100" : "opacity-0",
-                )}
                 onClick={onClose}
-            >
-                <button
-                    type="button"
-                    className={cn(
-                        "absolute bg-slate-400 flex h-9 items-center justify-center right-5 rounded-full top-5 w-9",
-                        "hover:bg-slate-500",
-                        "md:h-10 md:right-6 md:top-6 md:w-10",
-                        "lg:h-8 lg:right-5 lg:top-5 lg:w-8",
-                        "xl:h-10 xl:right-6 xl:top-6 xl:w-10",
-                    )}
-                    onClick={onClose}
-                >
-                    <Close
-                        color="var(--color-white)"
-                        className={cn(
-                            "h-5 w-5",
-                            "lg:h-4.5 lg:w-4.5",
-                            "xl:h-5 xl:w-5",
-                        )}
-                    />
-                </button>
-            </div>
+                className={cn(
+                    "backdrop-blur-md bg-slate-500/25 fixed inset-0 transition-opacity",
+                    isVisible ? "opacity-100" : "opacity-0",
+                )}
+            />
             <div className={cn(
-                "bg-sky-100 flex flex-col gap-5 items-center p-5 rounded-4xl shadow-2xl w-full z-10 max-w-sm",
-                isOpen ? "opacity-100 scale-100" : "opacity-0 scale-50",
-                "md:w-md md:max-w-none"
+                "bg-sky-100 flex flex-col gap-5 items-center origin-bottom p-5 rounded-4xl shadow-2xl transition-all w-full",
+                "md:gap-6 md:p-6 md:w-md",
+                isVisible ? "opacity-100 scale-100" : "opacity-0 scale-50",
             )}>
-                <div className={cn(
-                    "flex flex-col gap-4 items-center w-full",
-                )}>
-                    <ToggleButton
-                        isYearly={isYearly}
-                        onChange={setIsYearly}
-                    />
-                    <div className={cn(
-                        "bg-white flex flex-col gap-8 p-6 rounded-3xl w-full",
-                    )}>
-                        <div className={cn(
-                            "flex flex-col gap-5",
-                        )}>
-                            <span className={cn(
-                                "font-bold [text-box:trim-both_cap_alphabetic] text-sky-500",
-                                "md:text-[18px]"
-                            )}>
-                                PRO
-                            </span>
-                            <div className={cn(
-                                "flex gap-2 items-center",
-                            )}>
-                                <div className={cn(
-                                    "flex gap-1 items-end",
-                                )}>
-                                    <span className={cn(
-                                        "font-bold [text-box:trim-both_cap_alphabetic] text-xl",
-                                    )}>
-                                        ¥
-                                    </span>
-                                    <span className={cn(
-                                        "font-bold text-[32px] [text-box:trim-both_cap_alphabetic]",
-                                    )}>
-                                        {isYearly ? "500" : "625"}
-                                    </span>
-                                    <span className={cn(
-                                        "[text-box:trim-both_cap_alphabetic] text-sm",
-                                    )}>
-                                        / 月
-                                    </span>
-                                </div>
-                                <span className={cn(
-                                    "bg-slate-200 p-2 rounded-full text-[10px] [text-box:trim-both_cap_alphabetic] text-slate-500",
-                                )}>
-                                    {isYearly ? "¥6,000 年払い" : "月払い"}
-                                </span>
-                            </div>
-                        </div>
-                        <ul className={cn(
-                            "flex flex-col gap-6",
-                        )}>
-                            <li className={cn(
-                                "decoration-[20%] decoration-dotted decoration-slate-300 [text-box:trim-both_cap_alphabetic] text-slate-500 text-sm underline underline-offset-[50%]",
-                            )}>
-                                すべての機能を使用可能
-                            </li>
-                            <li className={cn(
-                                "decoration-[20%] decoration-dotted decoration-slate-300 [text-box:trim-both_cap_alphabetic] text-slate-500 text-sm underline underline-offset-[50%]",
-                            )}>
-                                高性能なAIモデル
-                            </li>
-                            <li className={cn(
-                                "decoration-[20%] decoration-dotted decoration-slate-300 [text-box:trim-both_cap_alphabetic] text-slate-500 text-sm underline underline-offset-[50%]",
-                            )}>
-                                生成は１日に100回まで
-                            </li>
-                            <li className={cn(
-                                "decoration-[20%] decoration-dotted decoration-slate-300 [text-box:trim-both_cap_alphabetic] text-slate-500 text-sm underline underline-offset-[50%]",
-                            )}>
-                                生成履歴の保存は無制限
-                            </li>
-                        </ul>
-                        <SubscribeButton plan={isYearly ? "yearly" : "monthly"} />
-                    </div>
-                </div>
+                <ToggleButton
+                    isYearly={isYearly}
+                    onChange={setIsYearly}
+                />
+                <PricingCard
+                    isYearly={isYearly}
+                    isPro={isPro}
+                />
                 <Link
-                    href="/commercial-low"
-                    className="text-[11px] [text-box:trim-both_cap_alphabetic] text-slate-500"
+                    href="/commercial-transactions"
+                    className={cn(
+                        "text-sm [text-box:trim-both_cap_alphabetic] text-slate-500 transition-colors",
+                        "hover:text-slate-800",
+                    )}
                 >
                     特定商取引法に基づく表記
                 </Link>
