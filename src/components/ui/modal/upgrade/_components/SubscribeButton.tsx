7c8e2f447d9596445e10b3eb672cebe19b57d193
yuki-devlab <yuki61051108@gmail.com>
2026-08-16 01:35:35 +0900

    refactor: update SubscribeButton.tsx

--- src/components/ui/modal/upgrade/_components/SubscribeButton.tsx
+++ src/components/ui/modal/upgrade/_components/SubscribeButton.tsx
@@ -3,21 +3,29 @@
 import { useState } from "react";
 import { ArrowRightAlt } from "@material-symbols-svg/react/w700";
 import { cn } from "@/lib/utils";
-import { createCheckoutSession } from "@/actions/stripe";
+import { createCheckoutSession } from "@/actions/stripe-checkout";
 
 type SubscribeButtonProps = {
-    plan: "monthly" | "yearly";
+    planType: "monthly" | "yearly";
+    isPro: boolean;
 };
 
-export default function SubscribeButton({ plan }: SubscribeButtonProps) {
-    const handleSubscribe = async () => {
-        try {
-            const res = await createCheckoutSession({ plan });
+export default function SubscribeButton({ planType, isPro }: SubscribeButtonProps) {
+    const [isLoading, setIsLoading] = useState(false);
 
-            if (res.url) {
+    const handleSubscribe = async () => {
+        setIsLoading(true);
+
+        try {
+            const res = await createCheckoutSession({ planType });
+
+            if (res?.url) {
                 window.location.href = res.url;
+            } else {
+                setIsLoading(false);
             }
         } catch (error) {
+            setIsLoading(false);
             throw new Error("エラーが発生しました");
         }
     };
@@ -25,22 +33,40 @@ export default function SubscribeButton({ plan }: SubscribeButtonProps) {
     return (
         <button
             type="button"
-            className={cn(
-                "w-full py-5 rounded-full bg-sky-500 flex items-center justify-center gap-6 relative",
-                "hover:bg-blue-500",
-            )}
             onClick={handleSubscribe}
+            disabled={isLoading || isPro}
+            className={cn(
+                "bg-sky-500 h-13 items-center rounded-full transition-colors",
+                "enabled:hover:bg-blue-500 disabled:cursor-not-allowed disabled:opacity-50",
+                isLoading
+                    ? "flex gap-2 justify-center"
+                    : "grid grid-cols-[1fr_auto_1fr]",
+            )}
         >
-            <span className={cn(
-                "font-bold text-sm [text-box:trim-both_cap_alphabetic] text-white",
-            )}>
-                Proプランをはじめる
-            </span>
-            <ArrowRightAlt
-                size={16}
-                color="var(--color-white)"
-                className="absolute right-10"
-            />
+            {isLoading ? (
+                <>
+                    <div className="animate-spin border-3 border-t-white border-white/25 h-5 rounded-full w-5" />
+                    <span className="font-semibold [text-box:trim-both_cap_alphabetic] text-white">
+                        Proプランをはじめる
+                    </span>
+                </>
+            ) : (
+                <>
+                    <div />
+                    <span className="font-semibold [text-box:trim-both_cap_alphabetic] text-white">
+                        {isPro ? "契約中" : "Proプランをはじめる"}
+                    </span>
+                    <div className="flex justify-center">
+                        {!isPro && (
+                            <ArrowRightAlt
+                                size={20}
+                                color="var(--color-white)"
+                                className="-translate-x-0.5"
+                            />
+                        )}
+                    </div>
+                </>
+            )}
         </button>
     );
 }
