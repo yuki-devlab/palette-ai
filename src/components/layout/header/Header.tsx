e0ec78295c514c4e5284cdbe6de1bb64948c3146
yuki-devlab <yuki61051108@gmail.com>
2026-08-16 22:55:24 +0900

    refactor: add sidebar props

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -4,6 +4,7 @@ import { getSubscriptionStatus } from "@/lib/subscription-status";
 import { cn } from "@/lib/utils";
 import AuthButton from "@/components/layout/header/_components/AuthButton";
 import HamburgerMenu from "@/components/layout/header/_components/HamburgerMenu";
+import Sidebar from "@/components/layout/sidebar/Sidebar";
 
 export default async function Header() {
     const session = await auth();
@@ -14,7 +15,12 @@ export default async function Header() {
             "bg-white flex items-center justify-between px-4 py-3 rounded-3xl shadow-sm w-full",
             "md:w-lg",
         )}>
-            <HamburgerMenu />
+            <HamburgerMenu>
+                <Sidebar className={cn(
+                    "flex flex-col gap-6 h-full rounded-3xl shadow-2xl",
+                    "lg:hidden",
+                )} />
+            </HamburgerMenu>
             <Link
                 href="/"
                 className="absolute left-1/2 -translate-x-1/2"
