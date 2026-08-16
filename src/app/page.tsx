cd1847915e4a8675fc60ada4943c16067cc30794
yuki-devlab <yuki61051108@gmail.com>
2026-08-17 04:04:27 +0900

    style: update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,35 +10,25 @@ export const metadata = createMetadata({
 export default function HomePage() {
     return (
         <div className={cn(
-            "flex flex-1 flex-col gap-10 items-center justify-center py-16",
+            "flex flex-col gap-10 items-center justify-center mx-auto",
             "xl:gap-12",
         )}>
             <div className={cn(
-                "flex flex-col gap-5 items-center",
-                "md:gap-6",
-                "xl:gap-8",
+                "flex flex-col gap-7 items-center px-5",
+                "md:px-8",
+                "xl:px-0",
             )}>
                 <div className={cn(
-                    "w-28 h-7 bg-slate-300",
-                    "md:w-32 md:h-8",
-                    "lg:w-28 lg:h-7",
-                    "xl:w-40 xl:h-10",
+                    "w-40 h-10 bg-slate-300",
                 )} />
-                <p className={cn(
-                    "[text-box:trim-both_cap_alphabetic] text-center text-slate-500 text-xs leading-loose",
-                    "md:text-sm",
-                    "lg:text-xs",
-                    "xl:text-base",
-                )}>
+                <p className="[text-box:trim-both_cap_alphabetic] text-center text-slate-500 leading-loose">
                     Palette AIは、ベースカラー・メインカラー・アクセントカラーを自動生成するAIツールです。
                     <br />
                     配色の知識がない方でも、簡単に生成することができます。
                 </p>
             </div>
             <ArrowIcon className={cn(
-                "h-12",
-                "md:h-14",
-                "lg:h-12",
+                "h-14",
                 "xl:h-16",
             )} />
             <GenerationMenu />
