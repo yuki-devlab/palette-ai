92c692b26010c8bd4c361b92d0190142e5890e3a
yuki-devlab <yuki61051108@gmail.com>
2026-08-16 22:56:24 +0900

    feat: add HamburgerMenu.tsx

--- src/components/layout/header/_components/HamburgerMenu.tsx
+++ src/components/layout/header/_components/HamburgerMenu.tsx
@@ -1,14 +1,41 @@
-import { Menu } from "@material-symbols-svg/react";
+"use client";
 
-export default function HamburgerMenu() {
+import { useState } from "react";
+import { Menu } from "@material-symbols-svg/react/w700";
+import SidebarDrawer from "@/components/ui/drawer/sidebar/SidebarDrawer";
+
+type HamburgerMenuProps = {
+    children: React.ReactNode;
+};
+
+export default function HamburgerMenu({ children }: HamburgerMenuProps) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openDrawer = () => {
+        setIsOpen(true);
+    };
+
+    const closeDrawer = () => {
+        setIsOpen(false);
+    };
+
     return (
-        <button
-            type="button"
-        >
-            <Menu
-                size={32}
-                color="var(--color-slate-400)"
-            />
-        </button>
+        <>
+            <button
+                type="button"
+                onClick={openDrawer}
+            >
+                <Menu
+                    size={32}
+                    color="var(--color-slate-400)"
+                />
+            </button>
+            <SidebarDrawer
+                isOpen={isOpen}
+                onClose={closeDrawer}
+            >
+                {children}
+            </SidebarDrawer>
+        </>
     );
 }
